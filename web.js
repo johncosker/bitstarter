7dfd88aea1c41e7b2a679f3ea97f856b90549166
John Cosker <cosker.john@gmail.com>
2013-07-10 19:17:14 +0000

    Modified web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
-
+var buf = fs.readFile('index.html')
 app.get('/', function(request, response) {
-  response.send('Derek\'s shitty');
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
