aa888f9fdccb6f6d25eeb3252ffd173473110a85
John Cosker <cosker.john@gmail.com>
2013-07-10 20:06:00 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var buffer = new Buffer(fs.readFileSync("index.html"), 'utf-8');
+
 app.get('/', function(request, response) {
-	var fs = require('fs');
-	var buffer = new Buffer(fs.readFileSync("index.html"), 'utf-8));
 	response.send(buffer.toString());
 });
 
