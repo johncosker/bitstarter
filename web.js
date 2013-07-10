a11f0da09eb502c131e10bc8b6375c04e15ad204
John Cosker <cosker.john@gmail.com>
2013-07-10 19:58:12 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,18 +1,13 @@
 var express = require('express');
-var fs = require('fs');
 var app = express.createServer(express.logger());
-var buf;
-fs.readFile('index.html', 'utf8', function (err, data){ 
-	if(err) {
-		return console.log(err);
-		}
-	buf.write(data, 'utf-8');
-});
+
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8'));
+	var fs = require('fs');
+	var buffer = new buffer();
+	response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
