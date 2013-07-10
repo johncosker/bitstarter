887a1bf2dfe7d550c3507ba2b62eecdd998e3c94
John Cosker <cosker.john@gmail.com>
2013-07-10 19:40:40 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var buf = fs.readFile('/index.html', 'utf8', function (err, data){ 
+var buf;
+fs.readFile('index.html', 'utf8', function (err, data){ 
 	if(err) {
 		return console.log(err);
 		}
+	buf.write(data);
 });
 app.get('/', function(request, response) {
   response.send(buf.toString('utf-8'));
