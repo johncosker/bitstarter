fac889de922e908a63ba6f9a87e69eca31f2e1bc
John Cosker <cosker.john@gmail.com>
2013-08-21 18:33:53 +0000

    Edited product desc in index.html

--- grader.js
+++ grader.js
@@ -26,7 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
+var URL_DEFAULT = "http://google.com";
+var sys = require('util');
+var rest = require('restler');
 var assertFileExists = function (infile) {
 	var instr = infile.toString();
 	if(!fs.existsSync(instr) ) {
@@ -38,6 +40,9 @@ var assertFileExists = function (infile) {
 
 var cheerioHtmlFile = function(htmlfile) {
 	return cheerio.load(fs.readFileSync(htmlfile));
+};
+var cheerioUrl = function(result) {
+	return cheerio.load(result);
 };
 var loadChecks = function(checksfile) {
 	return JSON.parse(fs.readFileSync(checksfile));
@@ -53,7 +58,20 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 	}
 	return out;
 };
-
+var checkUrl = function(result, checksfile) {
+	$ = cheerioUrl(result);
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	
+	for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	}
+	return out;
+};
+var assertURLExists = function(val) {
+	return val.toString();
+};
 var clone = function(fn) {
 	//Workaround for commander.js issue.
 	//http://stackoverflow.com/a/6772648
@@ -64,10 +82,19 @@ if (require.main == module) {
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <url>', 'Path to URL', clone(assertURLExists), URL_DEFAULT)
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+	if(program.url) {
+		rest.get(program.url).on('complete', function(result) {
+			var checkJson = checkUrl(result, program.checks);
+			var outJson = JSON.stringify(checkJson, null, 4);
+			console.log(outJson);
+			var outfile = "hw3p3_json.txt";
+			fs.writeFileSync(outfile, outJson);
+		});
 	} else {
-		exports.checkHtmlFile = checkHtmlFile;
+		var checkJson = checkHtmlFile(program.file, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	}
 }
